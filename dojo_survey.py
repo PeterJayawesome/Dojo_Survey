78eb2ae721b7476e554286d3a5f85eb35dc53b56
Peng Jiang <pengj@usc.edu>
2017-09-08 08:54:52 -0700

    with validation

---
+++ dojo_survey.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request,redirect,flash,session
+app = Flask(__name__)
+app.secret_key="djosurvey"
+@app.route('/')
+def survey():
+	return render_template('survey.html')
+
+@app.route('/result',methods=['POST'])
+def result():
+	name = request.form['name']
+	location = request.form['location']
+	language = request.form['language']
+	comment = request.form['comment']
+	if len(name)<1:
+		flash("Name could not be empty!")
+	elif len(comment)<1:
+		flash("Comment could not be empty!")
+	elif len(comment)>120:
+		flash("Comment could not be !")
+	else:
+		return render_template('result.html',name = name,location = location, language = language, comment = comment)
+	return redirect('/')
+
+app.run(debug=True)	
